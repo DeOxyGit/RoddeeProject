8d0a1f0fb6fe86f5c4a9347f32aec57e70caf16e
DeOxy <81672195+Ratchanon-Promsombut@users.noreply.github.com>
2024-07-06 20:07:52 +0700

    [EDIT] main

--- RoddeePoject/lib/main.dart
+++ RoddeePoject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myroddee/pages/getStarted_page.dart';
+import 'package:myroddee/pages/registerPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyGetStartedPage(),
+      home: const MyRegisterPage(),
     );
   }
 }
